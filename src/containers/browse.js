245759616fb65adfb2f482ff1468ca2e075939e1
Ayush Singh <ayushsops@gmail.com>
2022-02-02 21:27:41 +0530

    huh

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,5 +1,19 @@
-import React from 'react';
+import React, { useContext, useState, useEffect } from "react";
+import { SelectProfileContainer } from "./profiles";
+import { FirebaseContext } from "../context/firebase";
 
 export function BrowseContainer({ slides }) {
-  return <p>Hello</p>
+  const [profile, setProfile] = useState({});
+  const [loading, setLoading] = useState(true);
+  const { firebase } = useContext(FirebaseContext);
+  const user = firebase.auth().currentUser || {};
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, [profile.displayName]);
+
+  return <SelectProfileContainer user={user} setProfile={setProfile} />;
 }
+//5:29:43
